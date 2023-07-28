7217e9db00e7b6b58367d91eee17ec7ad1accbeb
Yana <yanochkabusinka@gmail.com>
2023-07-28 21:29:24 +0300

    init

---
+++ modyle1/tasks/task-1.js
@@ -0,0 +1,120 @@
+// "use strict";
+// let name = "Генератор защитного поля";
+// let price = 1000;
+// console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
+// price =2000
+// console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
+
+
+
+
+// "use strict";
+// const total = 100;
+// let ordered = 50;
+
+// if(ordered >total){
+//     console.log("На складе недостаточно товаров!");
+// }
+// else{
+// console.log("Заказ оформлен, с вами свяжется менеджер");
+// }
+// ordered =20
+// if(ordered >total){
+//     console.log("На складе недостаточно товаров!");
+// }
+// else{
+// console.log("Заказ оформлен, с вами свяжется менеджер");
+// }
+
+
+
+// const ADMIN_PASSWORD = 'jqueryismyjam';
+// let message;
+// const input = prompt("пароль");
+
+// if(input===null) 
+// { message = 'Отменено пользователем!'
+
+// }
+// else if( input === ADMIN_PASSWORD) {
+//     message = 'Добро пожаловать!'
+// }
+// else( message = 'Доступ запрещен, неверный пароль!')
+// alert (message);
+
+
+
+
+// const credits = 23580 ;
+// const pricePerDroid = 3000;
+// const quantity = prompt('Количество дроидов, для покупки?');
+// let totalPrice;
+// if(quantity===null) 
+// { console.log('Отменено пользователем!');}
+// else {
+//     totalPrice = pricePerDroid * Number(quantity);}
+
+//     if(totalPrice > credits) {
+    
+//         console.log('Недостаточно средств на счету!');}
+//         else {
+//             const balance = (credits - totalPrice);
+//             console.log(`Вы купили ${Number} дроидов, на счету осталось ${balance} кредитов.`);}
+
+
+
+
+
+
+
+
+// const  countryName = prompt ('Страна доставки?');
+// if (countryName === null) {
+//     alert('Отменино пользователем');}
+// else { 
+//     const country = countryName.toLowerCase();
+// let price ;
+// switch (country) {
+//     case'китай':
+//     price = 100;
+//  break;
+//  case'чили':
+//      price = 250 ;
+//      break;
+//  case'австралія':
+//     price = 170;
+//     break;
+//  case'індія':
+//     price= 80 ;
+//     break;
+//  case'ямайка':
+//     price = 120;
+//     break;
+
+//     default:
+//       alert("В вашей стране доставка не доступна");
+//       break;
+// }
+// if (price !== undefined) {
+//     alert(`Доставка в ${countryName} будет стоить ${price} кредитов`);
+//   }
+// }
+
+
+
+
+// let input;
+// let total = 0;
+
+
+// const clients = ["Mango", "Poly", "Ajax"];
+// clients[0] = "apples";
+// clients[1] = "Pango";
+// console.log(clients); // ["Kiwi", "Pango", "Ajax"]
+// console.log(clients.length)
+
+
+
+// const numbers = [1, 2, 3, 4, 5,6,7];
+// console.log(numbers.pop());
+// console.log(numbers.pop());
